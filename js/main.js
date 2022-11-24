350ae3727a3f888257b2d5276fa5665aafb0ba61
muminovaziz <muminov.me.2001@gmail.com>
2022-11-24 22:16:31 +0500

    added

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var userFirstName = prompt("Ismingizni kiriting");
+var test1 = prompt("BEM so'zining to'liq yozing");
+var test2 = prompt(" Bootstrapt da font-weight:700 ning chiqarish uchun  classga qanday nom bering kerak?");
+var test3 = prompt(" Cssni nech xikl usulda ulash mumkin?");
+var test4 = prompt(" Html so'zini to'liq yozing?");
+var test5 =prompt(" bootstraptni nech xil usulda ulash mumkin?");
+console.log("user_First_Name:" + " " + userFirstName + "\n" +
+    "mijozning_javobi:" + " " + test1 + "\n" +
+    "mijozning_javobi:" + " " + test2 + "\n" +
+    "mijozning_javobi:" + " " + test3 + "\n" +
+    "mijozning_javobi:" + " " + test4 + "\n" +
+    "mijozning_javobi:" + " " + test5 + "\n"
+); 
